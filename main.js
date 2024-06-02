21aef81dc5e9a59c1aa8a79eee5972892f7f79ba
Ademir Carter <60157299+AdemirCarter@users.noreply.github.com>
2024-06-02 17:45:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
+//-------------- mesa 1
 let valor1 = 0
 let mesa1 = document.querySelector('.titulo1')
 
 mesa1.textContent = 'mesa 1 - R$ ' + valor1
-
-//-------------- mesa 1
 
 let pastel1 = document.querySelector('.pastel1')
 let bolo1 = document.querySelector('.bolo1')
@@ -36,7 +35,7 @@ wolga1.addEventListener('click', function() {
 });
 
 bebida1_1.addEventListener('click', function() {
-    valor1 += 3
+    valor1 += 3.5
     mesa1.textContent = 'mesa 1 - R$ ' + valor1
 });
 
@@ -58,4 +57,66 @@ agua1.addEventListener('click', function() {
 limpar1.addEventListener('click', function() {
     valor1 = 0
     mesa1.textContent = 'mesa 1 - R$ ' + valor1
+});
+
+//-------------- mesa 2
+
+let valor2 = 0
+let mesa2 = document.querySelector('.titulo2')
+
+mesa2.textContent = 'mesa 2 - R$ ' + valor2
+
+let pastel2 = document.querySelector('.pastel2')
+let bolo2 = document.querySelector('.bolo2')
+let caldo2 = document.querySelector('.caldo2')
+let wolga2 = document.querySelector('.wolga2')
+let bebida1_2 = document.querySelector('.b250-2')
+let bebida2_2 = document.querySelector('.b600-2')
+let bebida3_2 = document.querySelector('.b1l-2')
+let agua2 = document.querySelector('.agua2')
+let limpar2 = document.querySelector('.limpar2')
+
+pastel2.addEventListener('click', function() {
+    valor2 += 3
+    mesa2.textContent = 'mesa 2 - R$ ' + valor2
+});
+
+bolo2.addEventListener('click', function() {
+    valor2 += 3
+    mesa2.textContent = 'mesa 2 - R$ ' + valor2
+});
+
+caldo2.addEventListener('click', function() {
+    valor2 += 2
+    mesa2.textContent = 'mesa 2 - R$ ' + valor2
+});
+
+wolga2.addEventListener('click', function() {
+    valor2 += 2
+    mesa2.textContent = 'mesa 2 - R$ ' + valor2
+});
+
+bebida1_2.addEventListener('click', function() {
+    valor2 += 3.5
+    mesa2.textContent = 'mesa 2 - R$ ' + valor2
+});
+
+bebida2_2.addEventListener('click', function() {
+    valor2 += 6
+    mesa2.textContent = 'mesa 2 - R$ ' + valor2
+});
+
+bebida3_2.addEventListener('click', function() {
+    valor2 += 8
+    mesa2.textContent = 'mesa 2 - R$ ' + valor2
+});
+
+agua2.addEventListener('click', function() {
+    valor2 += 2
+    mesa2.textContent = 'mesa 2 - R$ ' + valor2
+});
+
+limpar2.addEventListener('click', function() {
+    valor2 = 0
+    mesa2.textContent = 'mesa 2 - R$ ' + valor2
 });
